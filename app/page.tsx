5435d2f569717853fe8d8d1a1137710da1f55dcf
gerardmartin4 <148040174+gerardmartin4@users.noreply.github.com>
2024-03-06 10:31:27 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <header style={{ backgroundColor: '#007BFF', color: '#fff', padding: '20px', textAlign: 'center' }}>
+      <header style={{ backgroundColor: '#004080', color: '#fff', padding: '20px', textAlign: 'center' }}>
         <h1>Mi Supermarket</h1>
       </header>
       <nav style={{ backgroundColor: '#333', padding: '10px', textAlign: 'center' }}>
