3a140738c0e6af0cb2bbe980bdd9b5850282fa70
gerardmartin4 <148040174+gerardmartin4@users.noreply.github.com>
2024-03-10 18:51:01 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,6 @@ export default function Home() {
             height={250}
             style={{ margin: '0 auto' }} 
           />
-          <h3 style={{ marginTop: '10px' }}>Título del Producto</h3>
           <p>Dirige tu propio supermercado. Abastece las estanterías, fija los precios a tu gusto, acepta pagos, contrata personal, amplía y diseña tu tienda. Pedidos y entregas en línea, ladrones, seguridad, mercado local.</p>
           <a href="https://store.steampowered.com/app/2670630/Supermarket_Simulator/" target="_blank" rel="noopener noreferrer" style={{ display: 'block', margin: '10px auto', backgroundColor: '#004080', color: '#fff', padding: '8px 16px', borderRadius: '4px', textDecoration: 'none' }}>Comprar Videojuego</a>
         </article>
