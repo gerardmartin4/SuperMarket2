7de14451f3ef51a0abde70a46ec929944c7ae3be
gerardmartin4 <148040174+gerardmartin4@users.noreply.github.com>
2024-03-10 19:13:22 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,9 @@ export default function Home() {
           />
           <p>Dirige tu propio supermercado. Abastece las estanterías, fija los precios a tu gusto, acepta pagos, contrata personal, amplía y diseña tu tienda. Pedidos y entregas en línea, ladrones, seguridad, mercado local.</p>
           <a href="https://store.steampowered.com/app/2670630/Supermarket_Simulator/" target="_blank" rel="noopener noreferrer" style={{ display: 'block', margin: '10px auto', backgroundColor: '#004080', color: '#fff', padding: '8px 16px', borderRadius: '4px', textDecoration: 'none' }}>Comprar Videojuego</a>
-          <h2>Acerca de este juego</h2>
+        </article>
+        <article>
+         <h2>Acerca de este juego</h2>
             <h3>Administración de la tienda</h3>
               <p>Diseña tu tienda, optimizando la eficiencia y la estética. Determina dónde se exponen los productos, gestiona tus pasillos y garantiza una experiencia de compra fluida para tus clientes.</p>
         </article>
