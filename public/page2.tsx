f678267fc37e538ef5dcb8325731bcbdba48921c
gerardmartin4 <148040174+gerardmartin4@users.noreply.github.com>
2024-03-10 18:25:25 +0100

    Update page2.tsx

--- public/page2.tsx
+++ public/page2.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Page2  {
    return (
-    <div style={{ textAlign: 'center', padding: '20px' }}>
+     <div style={{ textAlign: 'center', padding: '20px' }}>
       <header style={{ backgroundColor: '#004080', color: '#fff', padding: '20px' }}>
         <h1>Mi Supermarket</h1>
       </header>
@@ -12,20 +12,27 @@ const Page2  {
           <a style={{ color: '#fff', textDecoration: 'none', padding: '0 10px' }}>Inicio</a>
         </Link>
         <Link href="/page2">
-          <a style={{ color: '#fff', textDecoration: 'none', padding: '0 10px' }}>Page 2</a>
+          <a style={{ color: '#fff', textDecoration: 'none', padding: '0 10px' }}>Acerca de este juego</a>
         </Link>
-         </nav>
+        <Link href="/system-requirements">
+          <a style={{ color: '#fff', textDecoration: 'none', padding: '0 10px' }}>Requisitos del Sistema</a>
+        </Link>
+      </nav>
       <section style={{ padding: '20px' }}>
-        <h2>ACERCA DE ESTE JUEGO</h2>
-        <p>
-          "Supermarket Simulator" es una escalofriante simulación en primera persona en la que cada detalle de la gestión de un supermercado cobra vida.
-        </p>
-        <p>
-          Diseña tu tienda, optimizando la eficiencia y la estética. Determina dónde se exponen los productos, gestiona tus pasillos y garantiza una experiencia de compra fluida para tus clientes.
-        </p>
-        {/* ... Añade más párrafos según sea necesario */}
+        <h2>Explora el Mundo del Supermercado</h2>
+        <article style={{ border: '1px solid #ddd', borderRadius: '8px', marginBottom: '20px', padding: '15px' }}>
+          <Image
+            src="/foto1.jpg" 
+            alt="Supermarket"
+            width={400}
+            height={250}
+            style={{ margin: '0 auto' }} 
+          />
+          <h3 style={{ marginTop: '10px' }}>Título del Producto</h3>
+          <p>Dirige tu propio supermercado. Abastece las estanterías, fija los precios a tu gusto, acepta pagos, contrata personal, amplía y diseña tu tienda. Pedidos y entregas en línea, ladrones, seguridad, mercado local.</p>
+          <a href="https://store.steampowered.com/app/2670630/Supermarket_Simulator/" target="_blank" rel="noopener noreferrer" style={{ display: 'block', margin: '10px auto', backgroundColor: '#004080', color: '#fff', padding: '8px 16px', borderRadius: '4px', textDecoration: 'none' }}>Comprar Videojuego</a>
+        </article>
       </section>
-      {/* ... Agrega más secciones según sea necesario */}
       <footer style={{ backgroundColor: '#333', color: '#fff', textAlign: 'center', padding: '10px', position: 'fixed', bottom: '0', width: '100%' }}>
         <p>Derechos de autor © 2024 Mi Supermarket Simulator</p>
       </footer>
