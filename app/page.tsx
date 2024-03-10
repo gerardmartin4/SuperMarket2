7f376af9666fc744fdeba7afa91869776deecf3e
gerardmartin4 <148040174+gerardmartin4@users.noreply.github.com>
2024-03-10 18:56:14 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div style={{ textAlign: 'center', padding: '20px' }}>
-      <header style={{ backgroundColor: '#004080', color: '#fff', padding: '20px' }}>
+    <div className="container">
+      <header className="header">
         <h1>Mi Supermarket</h1>
       </header>
-      <section style={{ padding: '20px' }}>
+      <section className="main-section">
         <h2>Explora el Mundo del Supermercado</h2>
-        <article style={{ border: '1px solid #ddd', borderRadius: '8px', marginBottom: '20px', padding: '15px' }}>
+        <article className="main-article">
           <Image
             src="/foto1.jpg" 
             alt="Supermarket"
@@ -18,10 +18,26 @@ export default function Home() {
             style={{ margin: '0 auto' }} 
           />
           <p>Dirige tu propio supermercado. Abastece las estanterías, fija los precios a tu gusto, acepta pagos, contrata personal, amplía y diseña tu tienda. Pedidos y entregas en línea, ladrones, seguridad, mercado local.</p>
-          <a href="https://store.steampowered.com/app/2670630/Supermarket_Simulator/" target="_blank" rel="noopener noreferrer" style={{ display: 'block', margin: '10px auto', backgroundColor: '#004080', color: '#fff', padding: '8px 16px', borderRadius: '4px', textDecoration: 'none' }}>Comprar Videojuego</a>
+          <a href="https://store.steampowered.com/app/2670630/Supermarket_Simulator/" target="_blank" rel="noopener noreferrer" className="buy-button">Comprar Videojuego</a>
+        </article>
+        
+        <article className="about-game">
+          <h3>ACERCA DE ESTE JUEGO</h3>
+          <p>"Supermarket Simulator" es una escalofriante simulación en primera persona en la que cada detalle de la gestión de un supermercado cobra vida.</p>
+          <p>Diseña tu tienda, optimizando la eficiencia y la estética. Determina dónde se exponen los productos, gestiona tus pasillos y garantiza una experiencia de compra fluida para tus clientes.</p>
+  
+          <p>Ordena las existencias utilizando un ordenador del juego. Desembala los productos, organízalos en tu almacén y colócalos en estanterías, frigoríficos y congeladores.</p>
+  
+          <p>Escanea artículos, acepta pagos en efectivo y con tarjeta de crédito, y asegúrate de que los clientes se van satisfechos con su experiencia de compra y pago.</p>
+  
+          <p>Navegue por las complejidades de un mercado en tiempo real. Compre productos cuando los precios bajen y determine los precios más vendidos para equilibrar la satisfacción del cliente con los márgenes de beneficio.</p>
+  
+          <p>A medida que acumules beneficios, plantéate reinvertir. Amplía el espacio físico de tu tienda, mejora los interiores y adáptate continuamente a las cambiantes demandas del mundo minorista.</p>
+  
+          <p>En "Supermarket Simulator", cada decisión cuenta. ¿Estarás a la altura de las circunstancias, transformando un modesto establecimiento en una potencia del comercio minorista, equilibrando al mismo tiempo la satisfacción del cliente y las finanzas?</p>
         </article>
       </section>
-      <footer style={{ backgroundColor: '#333', color: '#fff', textAlign: 'center', padding: '10px', position: 'fixed', bottom: '0', width: '100%' }}>
+      <footer className="footer">
         <p>Derechos de autor © 2024 Mi Supermarket Simulator</p>
       </footer>
     </div>
