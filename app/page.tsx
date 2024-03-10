3d3aa5945f949e39bb8d46c5b9f19ccd87ac473b
gerardmartin4 <148040174+gerardmartin4@users.noreply.github.com>
2024-03-10 17:03:57 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <div style={{ textAlign: 'center', padding: '20px' }}>
       <header style={{ backgroundColor: '#004080', color: '#fff', padding: '20px' }}>
-        <h1>Mi Supermarket Simulator</h1>
+        <h1>Mi Supermarket</h1>
       </header>
       <nav style={{ backgroundColor: '#333', padding: '10px', textAlign: 'center' }}>
         {/* ...otros enlaces... */}
       </nav>
       <section style={{ padding: '20px' }}>
-        <h2>Dirige tu propio supermercado. Abastece las estanterías, fija los precios a tu gusto, acepta pagos, contrata personal, amplía y diseña tu tienda. Pedidos y entregas en línea, ladrones, seguridad, mercado local.</h2>
+        <h2>Explora el Mundo del Supermercado</h2>
         <article style={{ border: '1px solid #ddd', borderRadius: '8px', marginBottom: '20px', padding: '15px' }}>
           <Image
             src="/foto1.jpg" // Añade la ruta correcta de tu imagen
@@ -19,8 +19,8 @@ export default function Home() {
             height={250}
             style={{ margin: '0 auto' }} // Centra la imagen horizontalmente
           />
-          <h3 style={{ marginTop: '10px' }}>Título del Producto</h3>
-          <p>Descubre los mejores productos para tu hogar. Desde alimentos frescos hasta artículos de limpieza.</p>
+          <p>Dirige tu propio supermercado. Abastece las estanterías, fija los precios a tu gusto, acepta pagos, contrata personal, amplía y diseña tu tienda. 
+            Pedidos y entregas en línea, ladrones, seguridad, mercado local.</p>
           <a href="https://store.steampowered.com/app/2670630/Supermarket_Simulator/" target="_blank" rel="noopener noreferrer" style={{ display: 'block', margin: '10px auto', backgroundColor: '#004080', color: '#fff', padding: '8px 16px', borderRadius: '4px', textDecoration: 'none' }}>Comprar Videojuego</a>
         </article>
         {/* Otra sección similar para la segunda imagen */}
