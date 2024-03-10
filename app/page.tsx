5828a525ceecb6abdcf88ee5a0c78176b178338a
gerardmartin4 <148040174+gerardmartin4@users.noreply.github.com>
2024-03-10 19:07:12 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,12 +20,6 @@ export default function Home() {
           <p>Dirige tu propio supermercado. Abastece las estanterías, fija los precios a tu gusto, acepta pagos, contrata personal, amplía y diseña tu tienda. Pedidos y entregas en línea, ladrones, seguridad, mercado local.</p>
           <a href="https://store.steampowered.com/app/2670630/Supermarket_Simulator/" target="_blank" rel="noopener noreferrer" style={{ display: 'block', margin: '10px auto', backgroundColor: '#004080', color: '#fff', padding: '8px 16px', borderRadius: '4px', textDecoration: 'none' }}>Comprar Videojuego</a>
         </article>
-        <article style={{ border: '1px solid #ddd', borderRadius: '8px', marginBottom: '20px', padding: '15px' }}>
-          <h2>Acerca de este juego</h2>
-          <p>"Supermarket Simulator" es un juego de simulación en primera persona en el que cada detalle de la gestión de un supermercado cobra vida.</p>
-          <h3>Administración de la tienda</h3>
-          <p>Diseña tu tienda, optimizando la eficiencia y la estética. Determina dónde se exponen los productos, gestiona tus pasillos y garantiza una experiencia de compra fluida para tus clientes.</p>
-        </article>
       </section>
       <footer style={{ backgroundColor: '#333', color: '#fff', textAlign: 'center', padding: '10px', position: 'fixed', bottom: '0', width: '100%' }}>
         <p>Derechos de autor © 2024 Mi Supermarket Simulator</p>
